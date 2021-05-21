ec3ecdccec7a0460289f844d88007742b0d4abb7
Rafael Alexandre <r.alexandre99@gmail.com>
2021-05-21 15:16:26 +0100

    added stage 2 tests

--- src/main/assets/scripts/stage2-tests/test4-stage2.sh
+++ src/main/assets/scripts/stage2-tests/test4-stage2.sh
@@ -2,7 +2,7 @@
 
 cd ../../../..
 mysql -u root -p123 < src/main/assets/sql/schema.sql
-konsole -e /bin/bash  --rcfile <(echo "mvn exec:java -Dexec.mainClass=\"Server.Server\" -Dexec.args=\"root 123 1 123456 server 6\"") &
+konsole -e /bin/bash  --rcfile <(echo "mvn exec:java -Dexec.mainClass=\"Server.Server\" -Dexec.args=\"root 123 1 123456 server 4\"") &
 konsole -e /bin/bash  --rcfile <(echo "mvn exec:java -Dexec.mainClass=\"Server.Server\" -Dexec.args=\"root 123 1 123456 server2 0\"") &
 konsole -e /bin/bash  --rcfile <(echo "mvn exec:java -Dexec.mainClass=\"Server.Server\" -Dexec.args=\"root 123 1 123456 server3 0\"") &
 konsole -e /bin/bash  --rcfile <(echo "mvn exec:java -Dexec.mainClass=\"Server.Server\" -Dexec.args=\"root 123 1 123456 server4 0\"") &
