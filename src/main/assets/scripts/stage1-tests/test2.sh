f1ed41e231eb12f3427eeb4b4ff630b83dd13242
Rafael Alexandre <r.alexandre99@gmail.com>
2021-05-21 16:41:15 +0100

    added report

--- src/main/assets/scripts/stage1-tests/test2.sh
+++ src/main/assets/scripts/stage1-tests/test2.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd ../../../..
+cd ../../../../..
 mysql -u root -p123 < src/main/assets/sql/schema.sql
 konsole -e /bin/bash  --rcfile <(echo "mvn exec:java -Dexec.mainClass=\"Server.Server\" -Dexec.args=\"afonso password 1 123456 0\"") &
 konsole -e /bin/bash  --rcfile <(echo "mvn exec:java -Dexec.mainClass=\"Client.Client\" -Dexec.args=\"user1 src/main/assets/grid_examples/grid2.txt 123456 1 1 1\"") &
